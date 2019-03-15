da277a552d06fdcb7c849bd82d4746c86221428b
Andrii Zadorozhnii <doxtop@synrc.com>
2019-03-15 18:05:48 +0200

    Handle moduleNotFound error with deferred compilation.

--- src/compile/mad_purs.erl
+++ src/compile/mad_purs.erl
@@ -3,8 +3,8 @@
 
 opt(Conf) -> mad_utils:get_value(purs_opts, Conf, []).
 
-compile(File,_Inc,Bin,Conf,Deps) ->
-    mad:info("==>Compile: ~p Dep:~p~n", [File, Deps]),
+compile(File,_Inc,Bin,Conf,Def) ->
+    mad:info("==> Compile: ~p deferred:~p~n", [File, Def]),
     DepsDir = mad_utils:get_value(deps_dir, Conf, ["deps"]),
     Opt = opt(Conf),
     Compiler= mad_utils:get_value(compiler, Opt, "purs"),
@@ -12,35 +12,30 @@ compile(File,_Inc,Bin,Conf,Deps) ->
     Bin2    = mad_utils:get_value(js_out,   Opt, Bin),
     NS      = mad_utils:get_value(namespace,Opt, "purescript-*"),
     Entry   = filename:basename(File, ".purs"),
-    DepE    = filename:basename(Deps, ".purs"),
-    Bundle  = filename:join([Bin2, Entry ++ ".js"]),
+    DefE    = filename:basename(Def, ".purs"),
+    Bundle  = filename:join([Bin2, case Def of [] -> Entry ++ ".js"; _ -> DefE ++ ".js" end]),
     Main    = mad_utils:get_value(main, Opt, []),
     Bundler = Compiler,
 
-    case sh:oneliner(io_lib:format("~s --version", [Compiler])) of 
+    case sh:oneliner(io_lib:format("~s --version", [Compiler])) of
         {_,0,Ver} ->
-        Line = case Deps of [] -> 
-            [atom_to_list(Compiler), "compile", File, filename:join([DepsDir, NS, "src", "**", "*.purs"]), "-o", Bin1];
-            M when Deps =/= File -> [atom_to_list(Compiler), "compile", File, M, filename:join([DepsDir, NS, "src", "**", "*.purs"]), "-o", Bin1];
-            _->[atom_to_list(Compiler), "compile", File, filename:join([DepsDir, NS, "src", "**", "*.purs"]), "-o", Bin1] end,
+        Compile = lists:filter(fun([]) -> false; (_) -> true end,
+            [atom_to_list(Compiler), "compile", File, Def, filename:join([DepsDir, NS, "src", "**", "*.purs"]), "-o", Bin1]),
 
-        mad:info("Run: ~p~n", [Line]),
-        case sh:oneliner(Line) of
-            {_,0,R} when Main =:= Entry orelse Main =:= DepE ->
+        case sh:oneliner(Compile) of
+            {_,0,R} when Main =:= Entry orelse Main =:= DefE ->
                 case sh:oneliner([atom_to_list(Bundler), "bundle", filename:join([Bin1, "**", "*.js"]), "-m", Entry, "--main", Main, "-o", Bundle]) of
                     {_,0,_} -> mad:info("~s to ~p with ~s-~s ~n", [R, Bundle, Compiler, Ver]), false;
                     {_,_,_R1} -> false
                 end;
-            {_,0,R} -> false; % nothing to compile
-            {_,_,R} -> 
-                mad:info("compilation error: ~p~n", [R]), 
-                ModuleNotFound = lists:any(fun(E) ->
-                case binary:split(E, [<<"Module ">>, <<" was not found.">>], [global, trim_all]) of 
-                    [_,Mod|_] -> true;
-                    _ -> false
-                end end, binary:split(R, [<<"Error ">>], [global,trim_all])),
-                if ModuleNotFound =:= true ->
-                    {postpone, filename:basename(File)}; % full path is actually ignored
-                true -> true end
+            {_,0,_R} -> false; % nothing to compile
+            {_,_,R} ->
+                mad:info("compilation error: ~p~n", [R]),
+                case binary:matches(R, [<<"Module ">>,<<" was not found">>], []) of % scope can be splited errors
+                    [{S1,L1},{S2,_}|_] ->
+                        Mod = binary:part(R,S1+L1,S2-S1-L1),
+                        {postpone, Mod};
+                    _ -> true
+                end
         end;
         {_,_,R} -> mad:info("Compiler not installed: ~p~n", [R]), true end.
